2f5c0503fda4fb80d95c57c7ab0035e06d053d4f
Tuutej <90647045+Tuutej@users.noreply.github.com>
2023-08-29 13:00:32 +0300

    ostoslista lisätty

--- App.js
+++ App.js
@@ -1,10 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [text, setText] = useState("");
+  const [data, setData] = useState([]);
+
+  const addButton = () => {
+    setData([...data, { key: text }]);
+    setText("");
+  };
+
+  const clearButton = () => {
+    setData([]);
+    setText("");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setText(text)}
+        value={text}
+      />
+      <View style={styles.buttonContainer}>
+        <Button onPress={addButton} title="Add" />
+        <View style={{ width: 20 }} />
+        <Button onPress={clearButton} title="Clear" />
+      </View>
+      <FlatList
+        style={styles.list}
+        data={data}
+        renderItem={({ item }) => <Text>{item.key}</Text>}
+        keyExtractor={(item, index) => index.toString()}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +49,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  input: {
+    marginTop: 50,
+    marginBottom: 5,
+    width: 200,
+    borderColor: "gray",
+    borderWidth: 1,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 20,
   },
 });
